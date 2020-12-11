1a88496cc8bbe201823296336bc9b5c5f0fac47a
Yazzen EL-Safadi <elsafadiyazzen@yahoo.com>
2020-12-11 11:26:31 +0100

    7th commit correction

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -8,7 +8,7 @@ const Logo = () => {
       <Tilt
         className="Tilt br3 shadow-5 mw5 mw7-ns center pa3 ph2-ns"
         options={{ max: 50 }}
-        style={{ height: 100, width: 250 }}
+        style={{ height: 100, width: 350 }}
       >
         <div className="Tilt-inner ph2-ns"> POKEFIGHT </div>
       </Tilt>
